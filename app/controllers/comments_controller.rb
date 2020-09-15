c3fdfaa48a36952d1d6e305e984162feba14073e
Shamel Lakin <christlife4u@gmail.com>
2020-09-15 13:14:22 -0400

    signup form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController 
-    http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+    # http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
     # def show 
         
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
     def create 
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
+        # binding.pry
         #create, link and save the comment
         redirect_to article_path(@article)
         #redirect to the original article object
