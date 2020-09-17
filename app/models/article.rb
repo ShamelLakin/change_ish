a356d6bb7eef1cee136769c3737c517c0e888c72
Shamel Lakin <christlife4u@gmail.com>
2020-09-17 17:28:39 -0400

    tweeks

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,6 @@ class Article < ApplicationRecord
     has_many :users, through: :comments
     #allows us to delete associated comments along with deleted articles
     validates :title, presence: true, length: { minimum: 5 }
+    validates :text, presence: true, length: {minimum: 2} 
     #article has many comments
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :article
   belongs_to :user
+
+  validates :commenter, presence: true
+  validates :body, presence: true, length: { minimum: 5 }
 end
