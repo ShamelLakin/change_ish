29a41c442bfd818dffce764aab8df77f8c85baaf
Shamel Lakin <christlife4u@gmail.com>
2020-09-20 10:17:12 -0400

    delete action needs fixn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,6 +22,7 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
         @comment.destroy
+        
         redirect_to article_path(@article)
         #find article locate comment @article.comments then destroy send to show
         #action for the article
