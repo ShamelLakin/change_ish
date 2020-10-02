f16225fb5acb41c2cd665001973fe707582d9125
Shamel Lakin <christlife4u@gmail.com>
2020-10-02 15:09:25 -0400

    delete action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,15 @@ class UsersController < ApplicationController
     
     def show 
     end
+
+    def create
+        User.create(user_params)
+      end
+     
+      private
+     
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+      end
     
 end
